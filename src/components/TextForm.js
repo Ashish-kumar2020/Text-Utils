57ec0916a655ac31e6cec3d6cc89fe64e41217d4
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2022-01-17 21:49:26 +0530

    Added word counter and preview

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -13,20 +13,26 @@ export default function TextForm(props) {
         let downText = text.toLowerCase();
         setText(downText);
     }
-    const [text,setText] = useState("Enter text here...");
+    const [text,setText] = useState("");
     // text = "new Text"  wrong way to change the state
     // setText("new Text") correct way to change the state
     return (
-        <div>
+        <>
+        <div className='container'>
             <h2>{props.heading}</h2>
             <div className="mb-3">
                 <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
             </div>
                 <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
-
                 <button className="btn btn-danger space" onClick={handleDownClick}>Convert to Lowercase</button>
-
-
         </div>
+        <div className="container">
+            <h2>Your Text Summary</h2>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>{0.008 * text.split(" ").length} Minutes read</p>
+            <h3>Preview your text here</h3>
+            <p>{text}</p>
+        </div>
+    </>
     )
 }
