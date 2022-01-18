0154cd438ec644ff1ba5f68de11eda8b665c8331
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2022-01-18 21:30:41 +0530

    Added Remove spaces, copy text feature and Improved Dark Mode

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -19,9 +19,21 @@ export default function TextForm(props) {
         setText(newTxt);
     }
 
+
     const handleToReverseText = () =>{
         let reverseText = text.split("").reverse().join("");
         setText(reverseText);
+    }
+
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "))
+    }
+
+    const handleCopy = () => {
+        var text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
     }
 
     const [text,setText] = useState("");
@@ -30,23 +42,24 @@ export default function TextForm(props) {
     return (
         <>
         <div className='container'>
-            <h2>{props.heading}</h2>
-            <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+            <div className="mb-3" style = {{color: props.mode === 'dark'?'white':'#042743'}}>
+                <h2>{props.heading}</h2>
+                <textarea className="form-control" value={text} onChange={handleOnChange} style = {{backgroundColor: props.mode === 'dark'?'grey': 'white', color: props.mode === 'dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
             </div>
                 <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
                 <button className="btn btn-danger space" onClick={handleDownClick}>Convert to Lowercase</button>
-                <button className="btn btn-danger space" onClick={handleToClearText}>Clear Text</button>
-                <button className="btn btn-success space" onClick={handleToReverseText}>Reverse Text</button>
-
+                <button className="btn btn-primary space" onClick={handleToClearText}>Clear Text</button>
+                <button className="btn btn-danger space" onClick={handleToReverseText}>Reverse Text</button>
+                <button className="btn btn-primary space" onClick={handleCopy}>Copy Text</button>
+                <button className="btn btn-danger space" onClick={handleExtraSpaces}>Remove Spaces</button>
 
         </div>
-        <div className="container">
-            <h2 className='my-2'>Your Text Summary</h2>
+        <div className="container" style = {{color:props.mode === 'dark'?'white':'#042743'}}>
+            <h2 className='my-2' >Your Text Summary</h2>
             <p>{text.split(" ").length} words and {text.length} characters</p>
             <p>{0.008 * text.split(" ").length} Minutes read</p>
             <h3 className='my-2'>Preview your text here</h3>
-            <p>{text}</p>
+            <p>{text.length>0 ? text: "Enter something to preview"}</p>
         </div>
     </>
     )
